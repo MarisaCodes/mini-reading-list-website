346c0bad381799b5df625e1c1992e674baaff2cc
MarisaCodes <marisakirisame955@gmail.com>
2022-06-29 01:04:11 +0400

    practiced DOM on app.js, nothing much

---
+++ app.js
@@ -0,0 +1,19 @@
+// const bookList = document.querySelector('#book-list');
+
+// console.log('book-list next element sibling is:',bookList.nextElementSibling);
+
+// console.log('book-list previous sibling is:',bookList.previousSibling);
+// console.log('book-list previous element sibling is:',bookList.previousElementSibling);
+
+
+// bookList.previousElementSibling.querySelector('p').innerHTML += "<br>Too cool for everyone else!";
+
+const myObj = {
+    key1:'hai',
+    key2:'I mean yes',
+    key3func:function potato() {
+        console.log('potatoes from soviet USSR');
+    }
+}
+
+console.log(Object.keys(myObj));
