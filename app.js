bf5669f9c01ff9be9fd497d14cd2e0ad27f6e4b5
MarisaCodes <marisakirisame955@gmail.com>
2022-06-29 15:01:48 +0400

    event bubbling fix

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const deleteButton = document.querySelectorAll('.delete');
-const books = document.querySelectorAll('.name');
-const addButton = document.querySelector('button');
-const ul = document.querySelector('ul');
-// console.log(deleteButton)
+const list = document.querySelector('#book-list ul');
+const addBtn = document.querySelector('button');
 
+list.addEventListener('click', function(event){
+    if(event.target.className == 'delete') {
+        event.target.parentElement.remove();
+    }
+})
 
-
-deleteButton.forEach(element => {
-    element.addEventListener('click',function(e){
-        e.target.parentElement.remove();
-    })
-});
+addBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    let newLi = document.createElement('li');
+    newLi.innerHTML = '<span class="name">Add book</span><span class="delete">delete</span>';
+    list.appendChild(newLi);
+})
